8134b4cfcfab405ae5ed6d8df994d68d97225c70
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-03-21 23:52:46 -0700

    adds render function for Cell component

--- components/Cell.js
+++ components/Cell.js
@@ -4,7 +4,21 @@ const React = require('react');
 
 class Cell extends React.Component {
   render () {
+    return (
+      <td className="cell">
+        <input 
+          value={this.props.cell} 
+          onChange={this.props.onChange} 
+          onFocus={this.props.onFocus} />
+      </td>
+    );
   }
+}
+
+Cell.proptypes = {
+  cell: React.PropTypes.string,
+  onChange: React.PropTypes.func,
+  onFocus: React.PropTypes.func
 }
 
 module.exports = Cell;
