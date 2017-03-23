9cd0a84df66521c95634e27f2bd425fde2cccf37
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2017-03-22 22:59:10 -0700

    adds addColumn and addRow actions

--- actions/index.js
+++ actions/index.js
@@ -1,9 +1,21 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault();
+  
+  this.setState({
+    table: this.state.table.map(row => [...row, ''])
+  })
 }
 
 function addRow (ev) {
+  ev.preventDefault();
+
+  const width = this.state.table[0].length;
+
+  this.setState({
+    table: [...this.state.table, new Array(width).fill('')]
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
