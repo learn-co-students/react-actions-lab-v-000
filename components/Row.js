dbf1c96391d9ec149fac2f5b467c56a5703361a7
Saturn226 <serenehopes@gmail.com>
2017-04-04 18:23:29 -0400

    Done.

--- components/Row.js
+++ components/Row.js
@@ -5,6 +5,18 @@ const Cell = require('./Cell');
 
 class Row extends React.Component {
   render () {
+    return(
+      <tr className="row">
+        {this.props.row.map((cell, column) =>
+          <Cell
+          cell={cell}
+          key={column}
+          onChange={this.props.onChange.bind(null,column)}
+          onFocus={this.props.onFocus.bind(null,column)}
+          />
+        )}
+      </tr>
+    )
   }
 }
 
