b0e61b4db9ca05027cab6b6790255f50cb9008f6
Scott Savage <snsavage@gmail.com>
2017-05-18 12:00:09 -0400

    Pass all lab specs

--- components/Row.js
+++ components/Row.js
@@ -5,5 +5,15 @@ import Cell from './Cell';
 
 export default class Row extends React.Component {
   render () {
+    const cells = this.props.row.map((cell, index) =>
+      <Cell key={index} cell={cell}
+        onChange={this.props.onChange.bind(null, index)}
+        onFocus={this.props.onFocus.bind(null, index)} />
+    );
+    return (
+      <tr className="row">
+        {cells}
+      </tr>
+    );
   }
 }
