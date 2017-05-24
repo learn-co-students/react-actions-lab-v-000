ea0d63d7c588fc4e99dc60055c7cf2636f9c89ee
walwoodr <walwoodr@gmail.com>
2017-05-24 18:59:56 -0500

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,76 @@
 'use strict';
 
+// done
 function addColumn (ev) {
+  ev.preventDefault();
+  this.state.table.map(function(row){
+    return row.push("")
+  });
+  this.setState({
+    table: this.state.table
+  })
 }
 
+// done
 function addRow (ev) {
+  ev.preventDefault();
+  var columnCount = this.state.table[0].length;
+  var newRow = [];
+  for (var i = columnCount; i > 0; i--){
+    newRow.push("")
+  };
+  this.state.table.push(newRow);
+  this.setState({
+    table: this.state.table
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  var cellValue = ev.target.value;
+  this.state.table[rowIndex][columnIndex] = cellValue;
+  this.setState({
+    table: this.state.table
+  })
 }
 
 function focusCell (rowIndex, columnIndex) {
+  var cellValue = this.state.table[rowIndex][columnIndex];
+  this.setState({
+    focused: cellValue
+  })
 }
 
+// done
 function blurCell () {
+  this.setState({
+    focused: null
+  })
 }
 
+// done
 function removeRow (ev) {
+  ev.preventDefault();
+  if (this.state.table.length > 1) {
+    this.state.table.pop();
+
+    this.setState({
+      table: this.state.table
+    })
+  }
 }
 
+// done
 function removeColumn (ev) {
+  ev.preventDefault();
+  if (this.state.table[0].length > 1) {
+    this.state.table.map(function(row){
+      return row.pop();
+    })
+
+    this.setState({
+      table: this.state.table
+    })
+  }
 }
 
 module.exports = {
--- components/Cell.js
+++ components/Cell.js
@@ -3,6 +3,22 @@
 import React from 'react';
 
 export default class Cell extends React.Component {
+  constructor(props){
+    super(props);
+    this.blurCell = this.props.blurCell;
+    this.state = {
+      value: this.props.cell
+    }
+  }
+
   render () {
+    const { changeCell, focusCell, blurCell, cell, rowId, cellId } = this.props;
+    const cellValue = this.state.value
+
+    return (
+      <td className="cell" id={rowId + "-" + cellId}>
+        <input value={cell} onChange={changeCell.bind(null, event)} onFocus={focusCell} onBlur={blurCell} />
+      </td>
+    )
   }
 }
