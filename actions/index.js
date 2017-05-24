f8c4277d8b4caead93f4684de2d613aee8061ecf
Jesse L Lawson <homestarflyer@gmail.com>
2017-05-24 14:12:49 -0400

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,68 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault();
+  this.setState({
+    table: this.state.table.map(row => [...row, ''])
+  });
 }
 
 function addRow (ev) {
+  ev.preventDefault();
+  let t = this.state.table[0].map(() => '')
+  this.setState({
+    table: [...this.state.table, t ]
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  const updatedRow = [...this.state.table[rowIndex]];
+  updatedRow.splice(columnIndex, 1, ev.target.value);
+
+  const updatedTable = [...this.state.table.slice()];
+  updatedTable.splice(rowIndex, 1, updatedRow);
+
+  this.setState({
+    table: updatedTable
+  });
 }
 
 function focusCell (rowIndex, columnIndex) {
+  this.setState({
+    focused: [rowIndex, columnIndex]
+  })
 }
 
 function blurCell () {
+  this.setState({
+    focused: null
+  })
 }
 
 function removeRow (ev) {
+  ev.preventDefault();
+
+  if (this.state.table.length > 1) {
+    const updatedTable = this.state.table
+      .slice(0, -1);
+
+    this.setState({
+      table: updatedTable
+    })
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault();
+
+  if (this.state.table[0].length > 1) {
+    const updatedTable = this.state.table
+      .map(row => row.slice(0, -1));
+
+    this.setState({
+      table: updatedTable
+    })
+  }
 }
 
 module.exports = {
--- components/Table.js
+++ components/Table.js
@@ -5,5 +5,19 @@ import Row from './Row';
 
 export default class Table extends React.Component {
   render () {
+    return(
+      <table className='table' onBlur={this.props.onBlur}>
+        <tbody>
+          {this.props.table.map((row, i) =>
+            <Row
+              key={i}
+              row={row}
+              onChange={this.props.onChange.bind(null, i)}
+              onFocus={this.props.onFocus.bind(null, i)}
+            />
+          )}
+        </tbody>
+      </table>
+    );
   }
 }
